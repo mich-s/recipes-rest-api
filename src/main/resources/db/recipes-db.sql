0c7d0912767b7f5162fc87fbb7c90e666deb089a
michal <michals22@gazeta.pl>
2018-09-25 14:06:49 +0200

    add sql script

---
+++ src/main/resources/db/recipes-db.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE  IF NOT EXISTS `recipes`;
+USE `recipes`;
+
+DROP TABLE IF EXISTS `recipe`;
+
+CREATE TABLE `recipe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(85) DEFAULT NULL,
+  `method` varchar(2500) DEFAULT NULL,
+  `ingredients` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+INSERT INTO `recipe` 
+VALUES 
+(1,'Smoky aubergine & red pepper salad', '1. Heat oven to 180C/160C fan/gas 4. Put the walnuts in a roasting tin and the pitta strips in another. Drizzle a little oil over the pitta strips and season with the za’atar and some salt and pepper. Place both trays in the oven and roast for 10 mins, then remove the walnuts. Roast the pitta strips for a further 3-5 mins until golden and crisp. Remove from the oven.
+
+2. Meanwhile, prick the aubergines and peppers all over with a fork. If you have a gas hob, turn on the rings to medium high (or better still, light the barbecue and move the coals to one side). Place the aubergines over the heat until they start to collapse, then turn them using tongs and cook the other side. Continue rotating until charred all over and soft all the way through. Repeat with the red peppers until charred and softened. If you don’t have a gas cooker or a barbecue, heat your grill to high (about 275C) and spread the pricked aubergines and peppers on a foil-lined baking tray, coating with a little oil. Grill for 10-15 mins on each side until charred and softened. Set aside.
+
+3. Remove the aubergines and peppers from the heat and allow to cool, then transfer to a chopping board. Carefully peel the skin from the peppers, halve and scoop out the seeds. Halve the aubergines and scrape the tender flesh into a bowl. Don’t worry if a little of the charred skin escapes into the bowl, it will add more flavour – but try to make sure there aren’t big shards of it in there.
+
+4. Tear the pepper into strips and put it in the bowl with the aubergine. Add almost all the chopped parsley, season, and stir in the olive oil and the vinegar. Arrange on a platter and crumble the walnuts and feta over the top. Garnish with some extra parsley and mint leaves.',
+'30g walnuts
+2 pitta breads, sliced into strips
+1 tsp za’atar
+2 tbsp rapeseed oil
+2 medium aubergines
+4-6 Romano peppers (depending on size)
+handful flatleaf parsley, finely chopped
+½ tbsp extra virgin olive oil
+1 tbsp sherry vinegar
+40g feta or vegetarian feta
+a few mint leaves, to garnish'),
+(2, 'Mango chutney', '1. Peel the mangoes and chop the flesh into blueberry-sized pieces. Pour the vinegar and sugar into a large pan and simmer gently, stirring until the sugar has dissolved. Increase the heat and bubble for 8-10 mins until reduced a little. Meanwhile, toast the cumin, coriander and cardamom in a dry pan until aromatic. 
+
+2. Tip the spices into a pestle and mortar and gently crush them, leaving the seeds with some texture. Remove the cardamom pods, leaving the seeds in the spice mix, and add to the vinegar mix along with the mangoes, the other ingredients and 2 tsp salt. Bubble over a medium heat for 1 hr 15 mins – 1 hr 35 mins, until thick and syrupy. Leave to sit for 10 mins.
+
+3. Transfer the chutney into 2-3 sterilised jars while still hot. Seal the jars and leave to cool, then add labels. Store in a cool place for up to 2 years – the chutney will be best eaten after a few months, when the flavours have melded and mellowed.',
+'6 firm but ripe mangoes (about 1½ kg)
+500ml white wine vinegar
+450g granulated sugar
+1 tbsp cumin seeds
+2 tsp coriander seeds
+10 cardamom pods
+2 tsp nigella seeds
+½ tsp cayenne pepper
+½ tsp turmeric
+3 garlic cloves, crushed
+8 whole cloves
+thumb-sized piece of ginger, grated
+1 fat red chilli, seeds removed and finely chopped'),
+(3, 'Easy white bread', '1. Mix the flour, salt and yeast in a large bowl. Make a well in the centre, then add the oil and water, and mix well. If the dough seems a little stiff, add 1-2 tbsp water, mix well then tip onto a lightly floured work surface and knead for around 10 mins. Once the dough is satin-smooth, place it in a lightly oiled bowl and cover with cling film. Leave to rise for 1 hour until doubled in size or place in the fridge overnight.
+
+2. Line a baking tray with baking parchment. Knock back the dough (punch the air out and pull the dough in on itself) then gently mould the dough into a ball. Place it on the baking parchment to prove for a further hour until doubled in size.
+
+3. Heat oven to 220C/fan 200C/gas 7. Dust the loaf with flour and cut a cross about 6cm long into the top of the loaf with a sharp knife. Bake for 25-30 mins until golden brown and the loaf sounds hollow when tapped underneath. Cool on a wire rack.',
+'500g strong white flour, plus extra for dusting
+2 tsp salt
+7g sachet fast-action yeast
+3 tbsp olive oil
+300ml water');
